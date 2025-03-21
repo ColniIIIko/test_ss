512eb93288e463b4715ff0fd59cfaebe5841b891
Ivan Savickiy <isavitskiy12@gmail.com>
2025-03-21 03:24:56 +0300

    aaa

--- src/main.js
+++ src/main.js
@@ -1,24 +1,79 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { loadStripe } from '@stripe/stripe-js';
+import './style.css';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+import Stripe from 'stripe';
 
-setupCounter(document.querySelector('#counter'))
+const stripeServ = Stripe(
+  'sk_test_51R4JcsBMz4CEt0vZRydsij2VBw0AO7SnvF4zbZyPyKABh7p2KOZAAyHdY9WrLNAHe8z6cVElsi4mvoSJIL3Sobdz00k2QERnaV'
+);
+
+async function start() {
+  const stripe = await loadStripe(
+    'pk_test_51R4JcsBMz4CEt0vZbjSCJtBrRSgWO9FDyObpKA0SgIQqw1Me04FleOR15KbwerqrZUoCccMGCFo5aeVbsT4IDyIs00L8d75hwm'
+  );
+  const options = {
+    mode: 'payment',
+    amount: 1099,
+    currency: 'usd',
+    // Customizable by using the Appearance API.
+    appearance: {
+      /*...*/
+    },
+  };
+
+  // Set up Stripe.js and Elements to use in checkout form.
+  const elements = stripe.elements(options);
+
+  const expressCheckoutElement = elements.create('expressCheckout');
+  expressCheckoutElement.mount('#app');
+
+  const customer = await stripeServ.customers.create({
+    name: 'Test test',
+    email: 'isavitskiy12@gmail.com',
+  });
+
+  const intent = await stripeServ.paymentIntents.create({
+    // To allow saving and retrieving payment methods, provide the Customer ID.
+    customer: customer.id,
+    amount: 1099,
+    currency: 'usd',
+    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+    automatic_payment_methods: { enabled: true },
+  });
+
+  const handleError = (error) => {
+    const messageContainer = document.querySelector('#error-message');
+    messageContainer.textContent = error.message;
+  };
+
+  expressCheckoutElement.on('confirm', async (event) => {
+    const { error: submitError } = await elements.submit();
+    if (submitError) {
+      handleError(submitError);
+      return;
+    }
+
+    const clientSecret = intent.client_secret;
+
+    const { error, paymentIntent } = await stripe.confirmPayment({
+      // `elements` instance used to create the Express Checkout Element
+      elements,
+      // `clientSecret` from the created PaymentIntent
+      clientSecret,
+      redirect: 'if_required',
+      confirmParams: {
+        return_url: window.location.href,
+      },
+    });
+
+    if (error) {
+      // This point is only reached if there's an immediate error when
+      // confirming the payment. Show the error to your customer (for example, payment details incomplete)
+      handleError(error);
+    } else {
+      console.log(paymentIntent);
+    }
+  });
+}
+
+start();
